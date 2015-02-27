ac60294e6ba3f7c04f437feb8cedeb76233c0054
Mazillka <mazillko@gmail.com>
2015-02-27 16:11:23 +0200

    8
    code cleanup

--- Google Services/popup.js
+++ Google Services/popup.js
@@ -1,29 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-	document.getElementById("search").addEventListener("click", function(){
-		var tab = window.open('https://google.com', '_blank'); 
-        tab.focus();
-	});
-
-	document.getElementById("play").addEventListener("click", function(){
-		var tab = window.open('https://play.google.com', '_blank'); 
-        tab.focus();
-	});
-	
-	document.getElementById("drive").addEventListener("click", function(){
-		var tab = window.open('https://drive.google.com', '_blank'); 
-        tab.focus();
-	});
-
-	document.getElementById("translate").addEventListener("click", function(){
-		var tab = window.open('https://translate.google.com', '_blank'); 
-        tab.focus();
-	});
-
-	document.getElementById("youtube").addEventListener("click", function(){
-		var tab = window.open('https://youtube.com', '_blank'); 
-        tab.focus();
-	});
-	
 	document.getElementById("gmail").addEventListener("click", function(){
 		chrome.tabs.getAllInWindow(null, function(tabs) {
 			for (var i = 0; i < tabs.length; i++) {
